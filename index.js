f67bb81ebf752fc039d2603526ffe2ec1671c560
Rafael Ribeiro <rafaelr.alves@yahoo.com.br>
2024-04-30 05:55:37 -0300

    feat: add route file to task entity

--- index.js
+++ index.js
@@ -1,67 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const TaskRouter = require('./src/routes/task.routes.js');
 
 const connectToDatabase = require('./src/database/mongoose.database.js');
 
-const TaskModel = require('./src/models/task.model.js');
-
 dotenv.config();
-
-connectToDatabase();
-
 const app = express();
 app.use(express.json());
 
-app.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await TaskModel.find({});
-        res.status(200).send(tasks);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
+connectToDatabase();
 
-app.get('/tasks/:id', async (req, res) => {
-    try {
-        const taskId = req.params.id;
-
-        const task = await TaskModel.findById(taskId);
-
-        if (!task) {
-            return res.status(404).send('Task not found');
-        }
-        return res.status(200).send(task);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-app.post('/tasks', async (req, res) => {
-    try {
-        const newTask = new TaskModel(req.body);
-
-        await newTask.save();
-
-        res.status(201).send(newTask);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-app.delete('/tasks/:id', async (req, res) => {
-    try {
-        const taskId = req.params.id;
-
-        const deletedTask = await TaskModel.findByIdAndDelete(taskId);
-
-        if (!deletedTask) {
-            return res.status(404).send('Task not found');
-        }
-
-        res.status(200).send(deletedTask);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
+app.use('/tasks', TaskRouter);
 
 app.listen(8000, () => console.log('Listen on port 8000'));
